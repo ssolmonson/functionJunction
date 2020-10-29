7ffd5e1402a0adf0709d9d9eea4e65fd134a615d
Scott Solmonson <ssolmonson63@gmail.com>
2020-10-29 16:55:19 -0400

    Add leading zero to hex value
    Fixed the output for hexadecimal values of r, g, or b to add a leading
    zero if the strings length was equal to 1.

--- Code Wars/JS/rgbToHex.js
+++ Code Wars/JS/rgbToHex.js
@@ -1,6 +1,8 @@
 function rgb(r, g, b){
   // complete this function
+  // hexDec = r.toString(16).toUpperCase() + g.toString(16).toUpperCase() + b.toString(16).toUpperCase()
 
+  // initiate variables for the rgb hex values
   hexDecR = null
   hexDecG = null
   hexDecB = null
@@ -26,8 +28,18 @@ function rgb(r, g, b){
     hexDecB = b.toString(16).toUpperCase()
   }
 
-  // hexDec = r.toString(16).toUpperCase() + g.toString(16).toUpperCase() + b.toString(16).toUpperCase()
+  // add a leading 0 if the hexadecimal string is a single digit
+  if (hexDecR.length = 1) {
+    hexDecR = hexDecR.padStart(2, hexDecR)
+  }
 
+  if (hexDecG.length = 1) {
+    hexDecG = hexDecG.padStart(2, hexDecG)
+  }
+
+  if (hexDecB.length = 1) {
+    hexDecB = hexDecB.padStart(2, hexDecB)
+  }
 
 
   return hexDecR + hexDecG + hexDecB
