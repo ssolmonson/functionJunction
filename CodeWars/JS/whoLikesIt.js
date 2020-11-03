d289f7bf3f656497bcf18cf23a31be112407c5b8
Scott Solmonson <ssolmonson63@gmail.com>
2020-11-03 17:42:12 -0500

    Add function for an array to display likes
    Created a function that will display names and people who like a message
    similar to how social media platforms may display this information.

--- CodeWars/JS/whoLikesIt.js
+++ CodeWars/JS/whoLikesIt.js
@@ -2,17 +2,32 @@ function likes(names) {
   // TODO
 
   // if no names
-  // return "no one likes this"
+  if names.length === 0 {
+    // return "no one likes this"
+    return "no one likes this"
+  }
 
   // if one name
-  // return "<name> likes this"
+  if names.length === 1 {
+    // return "<name> likes this"
+    return names[0] + " likes this"
+  }
 
   // if two names
-  // return "<name> and <name> like this"
+  if names.length === 2 {
+    // return "<name> and <name> like this"
+    return names[0] + " " + names[1] + " like this"
+  }
 
   // if three
-  // return "<name>, <name> and <name> like this"
+  if names.length === 3 {
+    // return "<name>, <name> and <name> like this"
+    return names[0] + " " + names[1] + " " + names[2] + " like this"
+  }
 
   // if more
-  // return "first two names and <length of rest of amount of names> like this"
+  if names.length > 3 {
+    // return "first two names and <length of rest of amount of names> like this"
+    return names[0] + " " + names[1] + names.split + " like this"
+  }
 }
