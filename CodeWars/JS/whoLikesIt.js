58b57126fee3979dda3e0df4f7878dfa58d3e84d
Scott Solmonson <ssolmonson63@gmail.com>
2020-11-03 17:53:16 -0500

    Fix entry

--- CodeWars/JS/whoLikesIt.js
+++ CodeWars/JS/whoLikesIt.js
@@ -2,35 +2,33 @@ function likes(names) {
   // TODO
 
   // if no names
-  if names.length === 0 {
+  if (names.length === 0) {
     // return "no one likes this"
     return "no one likes this"
   }
 
   // if one name
-  if names.length === 1 {
+  if (names.length === 1) {
     // return "<name> likes this"
     return names[0] + " likes this"
   }
 
   // if two names
-  if names.length === 2 {
+  if (names.length === 2) {
     // return "<name> and <name> like this"
-    return names[0] + " " + names[1] + " like this"
+    return names[0] + " and " + names[1] + " like this"
   }
 
   // if three
-  if names.length === 3 {
+  if (names.length === 3) {
     // return "<name>, <name> and <name> like this"
-    return names[0] + " " + names[1] + " " + names[2] + " like this"
+    return names[0] + ", " + names[1] + " and " + names[2] + " like this"
   }
 
   // if more
-  if names.length > 3 {
+  if (names.length > 3) {
+    const newNames = names.slice(2)
     // return "first two names and <length of rest of amount of names> like this"
-    return names[0] + " " + names[1] + names.split + " like this"
+    return names[0] + ", " + names[1] + " and " + newNames.length + " others like this"
   }
 }
-
-// Review code to refractor
-// Solves the issue but should be worked on to prevent uncessary repitition
