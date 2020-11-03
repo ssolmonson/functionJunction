35b8ad3d3fa8c7188fb3199bc92629211b679acf
Scott Solmonson <ssolmonson63@gmail.com>
2020-11-03 17:58:18 -0500

    Remove 'if' statement and unnecessary variable
    The final 'if' was not needed and was turned into a 'return'. The
    newNames variable was removed in favor of removing two for the length of
    the array to find how many others liked it.

--- CodeWars/JS/whoLikesIt.js
+++ CodeWars/JS/whoLikesIt.js
@@ -6,29 +6,22 @@ function likes(names) {
     // return "no one likes this"
     return "no one likes this"
   }
-
   // if one name
   if (names.length === 1) {
     // return "<name> likes this"
     return names[0] + " likes this"
   }
-
   // if two names
   if (names.length === 2) {
     // return "<name> and <name> like this"
     return names[0] + " and " + names[1] + " like this"
   }
-
   // if three
   if (names.length === 3) {
     // return "<name>, <name> and <name> like this"
     return names[0] + ", " + names[1] + " and " + names[2] + " like this"
   }
-
-  // if more
-  if (names.length > 3) {
-    const newNames = names.slice(2)
-    // return "first two names and <length of rest of amount of names> like this"
-    return names[0] + ", " + names[1] + " and " + newNames.length + " others like this"
-  }
+  // if any more return first two names and the amount of other names
+  // return "first two names and <length of rest of amount of names> others like this"
+  return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"
 }
