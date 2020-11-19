94fe1abf0a57426622b901f40b0669c9875f9e15
Scott Solmonson <ssolmonson63@gmail.com>
2020-11-18 23:46:32 -0500

    Add start of solution for 'if else' problem

---
+++ HackerRank/JS/ifElseDayTwo.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getGrade(score) {
+    let grade;
+    if (25 < score <= 30) {}
+    else if (25 < score <= 30) {
+        grade = 'A'
+    }
+    else if (20 < score <= 25) {
+        grade = 'B'
+    }
+    else if (15 < score <= 20) {
+        grade = 'C'
+    }
+    else if (10 < score <= 15) {
+        grade = 'D'
+    }
+    else if (5 < score <= 10) {
+        grade = 'E'
+    }
+    else if (0 <= score <= 5) {
+        grade = 'F'
+    }
+
+    return grade;
+}
