375b89e324d3e6e129818cd439a837061ab68cd0
Scott Solmonson <ssolmonson63@gmail.com>
2021-01-27 23:37:45 -0500

    Plan out moving zeros to end function

---
+++ HackerRank/JS/zerosToEnd.js
@@ -0,0 +1,11 @@
+var moveZeros = function (arr) {
+  // 
+  // let newarray
+  // filter array for 0
+  //
+  // run through array remove 0's
+  //
+  // after loop through the original array
+  //
+  // concat removedarray with newarray
+}
