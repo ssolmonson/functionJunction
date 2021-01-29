e17f1797cd11ac2e7fcefae09fb2774ee4585408
Scott Solmonson <ssolmonson63@gmail.com>
2021-01-28 23:33:02 -0500

    Edit zerosToEnd and add notes/thoughts
    Corrected some notation from the code that it was missing from after
    being copied over from Code Wars. Also added notes about my personal
    review of the code and how to refractor. This function successfully
    completed all tests.

--- CodeWars/JS/zerosToEnd.js
+++ CodeWars/JS/zerosToEnd.js
@@ -1,14 +1,22 @@
+
 var moveZeros = function (arr) {
-  var moveZeros = function (arr) {
-    // filter array for 0's
-    const filterArray = arr.filter(zero => zero === 0)
-    //
-    // run through array remove 0's
-    let i
-    while ((i = arr.indexOf(0)) != -1) {
-      arr.splice(i, 1)
-    }
-    // after loop through the original array
-    // concat filterArray with zero removed array
-    return arr.concat(filterArray)
+  // filter array for 0's
+  const filterArray = arr.filter(zero => zero === 0)
+  //
+  // run through array remove 0's
+  let i
+  while ((i = arr.indexOf(0)) !== -1) {
+    arr.splice(i, 1)
+  }
+  // after loop through the original array
+  // concat filterArray with zero removed array
+  return arr.concat(filterArray)
 }
+
+// Successfully passed all tests and has been submitted.
+// Alternatively when reviewing this function
+// it occured to me that another filter could be put in place of the while loop
+// i.e.
+// const removeZeros = arr.filter(zero => zero !== 0)
+// and concat together both filtered arrays
+// this would shorten it by 3 lines
