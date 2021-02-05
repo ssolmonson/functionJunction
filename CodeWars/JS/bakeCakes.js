84061b5f5556158b24328061073f5d8cae794df4
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-04 23:58:08 -0500

    Add structure

--- CodeWars/JS/bakeCakes.js
+++ CodeWars/JS/bakeCakes.js
@@ -1,4 +1,5 @@
 function cakes(recipe, available) {
   // TODO: insert code
   // compare two objects to return how many of one cant be made from the other
+  // Need to compare first set with certain in second set
 }
