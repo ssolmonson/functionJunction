86e22b95efafc94fdde1452457f85620d28dd5d4
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-10 23:25:36 -0500

    Create initial solution
    Added code for the first part of the solution. Started with multiple
    lines, before realizing it could be converted into one simplified piece.
    
    Next addition will focus on providing a solution should the amount of
    left rotations be a number greater than the length of the array.

---
+++ HackerRank/JS/leftRotation.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    // determine if d is greater than array length
+    // if greater divide d by length
+    // using remainder as split point
+    // split array into two arrays at position d
+    //
+    // let splitArr = a.slice(d)
+    // let origArr = a.slice(0, d)
+    // return splitArr.concat(origArr)
+    //
+    // simplified one line of above
+    const newArr = a.slice(d).concat(a.slice(0, d))
+    return newArr
+    // concat both arrays
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
