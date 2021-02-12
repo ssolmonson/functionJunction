8a3cdeabe4080d6e615f9815f8bab55a416638de
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-11 21:49:47 -0500

    Complete function for bakeCakes
    Completed function for bakeCakes and passed first couple tests. Still
    needs revision to pass subsequent tests. This might be due to how the
    'available' keys are ordered.

--- CodeWars/JS/bakeCakes.js
+++ CodeWars/JS/bakeCakes.js
@@ -1,11 +1,13 @@
 function cakes(recipe, available) {
   // TODO: insert code
   // Create array to place keys of recipe to filter the needed keys of available
-  const ingredients = Object.keys(recipe)
+  const recipeKeys = Object.keys(recipe)
+  // Create an array for the values of the keys
+  const recipeValues = Object.values(recipe)
   // creating new object
   // filter keys in available for keys in recipe
   const filteredAvail = Object.keys(available)
-    .filter(key => ingredients.includes(key))
+    .filter(key => recipeKeys.includes(key))
     .reduce((obj, key) => {
       obj[key] = available[key]
       return obj
@@ -16,10 +18,17 @@ function cakes(recipe, available) {
     //   [key]: available[key]
     //   }
     // }, {})
+  const filteredAvailKeys = Object.keys(filteredAvail)
   // compare two object values for each key
-  // ex: recipe.name === available.name
-  // values will be divided into each other
-  // find least divisible whole number of compared object values and return
+  if (recipeKeys.length !== filteredAvailKeys.length) {
+    return 0
+  } else {
+    let howMany = []
+    for (let i = 0; i < filteredAvailKeys.length; i++) {
+      howMany.push(Math.floor(filteredAvail[filteredAvailKeys[i]] / recipeValues[i]))
+    }
+    // values will be divided into each other
+    // find least divisible whole number of compared object values and return
+    return Math.min(...howMany)
+  }
 }
-  // could use Object.entries()
-  // const ingredients = Object.entries(recipe)
