2a6c85b99b072ba2256208f96084647856f65845
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-11 20:59:23 -0500

    Add better structure and first starting piece
    Created a way to filter the objects for only the necessary keys.

--- CodeWars/JS/bakeCakes.js
+++ CodeWars/JS/bakeCakes.js
@@ -1,5 +1,25 @@
 function cakes(recipe, available) {
   // TODO: insert code
-  // compare two objects to return how many of one cant be made from the other
-  // Need to compare first set with certain in second set
+  // Create array to place keys of recipe to filter the needed keys of available
+  const ingredients = Object.keys(recipe)
+  // creating new object
+  // filter keys in available for keys in recipe
+  const filteredAvail = Object.keys(available)
+    .filter(key => ingredients.includes(key))
+    .reduce((obj, key) => {
+      obj[key] = available[key]
+      return obj
+    }, {})
+    // .reduce((obj, key) => {
+    //   return {
+    //   ...obj,
+    //   [key]: available[key]
+    //   }
+    // }, {})
+  // compare two object values for each key
+  // ex: recipe.name === available.name
+  // values will be divided into each other
+  // find least divisible whole number of compared object values and return
 }
+  // could use Object.entries()
+  // const ingredients = Object.entries(recipe)
