9d60fc194d52ff4b9c1586ed06ef52a10fe7159a
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-16 23:57:50 -0500

    Add plan for next steps to develop function

--- CodeWars/JS/sumOfIntervals.js
+++ CodeWars/JS/sumOfIntervals.js
@@ -7,6 +7,11 @@ function sumIntervals(intervals){
   const maxInt = Math.max(...merged)
   const minInt = Math.min(...merged)
   return maxInt - minInt
+  // Define ranges of intervals
+  // Find intervals which overlap
+  // Merge overlapping intervals based on highest and lowest of the two numbers
+  // Find the difference of all
+  // Add together
 }
 // This will solve, only is all intervals are overlapping
 // First simple solution which passed the majority of tests,
