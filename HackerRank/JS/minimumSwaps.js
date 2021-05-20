0a4f6af620ad12d7bf42b6cd1c8094a76ba25156
Scott Solmonson <ssolmonson63@gmail.com>
2021-05-20 03:54:12 -0400

    Create function to sort and count swaps

--- HackerRank/JS/minimumSwaps.js
+++ HackerRank/JS/minimumSwaps.js
@@ -26,16 +26,39 @@ function readLine() {
 
 // Complete the minimumSwaps function below.
 function minimumSwaps(arr) {
+    // Define the variable to be counted and set at zero
     let swaps = 0
 
-    arr.sort((a, b) => {
-        if (a > b) {
-            swaps++
-            return a - b
-        } else {}
-    })
+    // Define 'i' for loop
+    // let i = 0
 
+    // Loop through array
+    for (let i = 0; i < arr.length; i++) {
+      // While current index is not in the correct position
+      while (arr[i] !== i + 1) {
+        // Assign element located out of order, assign it tempSpot
+        let tempSpot = arr[i]
+        // Move element to correct spot
+        arr[i] = arr[tempSpot - 1]
+        arr[tempSpot - 1] = tempSpot
+        // Add to swap count
+        swaps++
+      }
+    }
+
+    // return the number of swaps
     return swaps
+
+    // Basic function to test
+    // As thought, this sorts without without swaps, resulting in lower than intended output in most cases
+    // arr.sort((a, b) => {
+    //     if (a > b) {
+    //         swaps++
+    //         return a - b
+    //     } else {}
+    // })
+    //
+    // return swaps
 
 }
 
