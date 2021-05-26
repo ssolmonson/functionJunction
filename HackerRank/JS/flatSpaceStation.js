e5d3f8642c46daebc87df6654ec03bd3a05b2f29
Scott Solmonson <ssolmonson63@gmail.com>
2021-05-25 22:44:46 -0400

    Add setup for space station problem

---
+++ HackerRank/JS/flatSpaceStation.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the flatlandSpaceStations function below.
+function flatlandSpaceStations(n, c) {
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nm = readLine().split(' ');
+
+    const n = parseInt(nm[0], 10);
+
+    const m = parseInt(nm[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = flatlandSpaceStations(n, c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
