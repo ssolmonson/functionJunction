860ccce3f5b83a5b316cbfeb9fb94b145beb5aa7
Scott Solmonson <ssolmonson63@gmail.com>
2022-01-18 20:21:03 -0500

    Setup for Single Word Pig Latin problem

---
+++ CodeWars/JS/singleWordPL.js
@@ -0,0 +1,17 @@
+function pigLatin(string){
+  //
+}
+
+// Tests:
+//
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+//
+// describe("Sample Tests", () => {
+//   it("Test", () => {
+//     assert.strictEqual(pigLatin("map"), "apmay");
+//     assert.strictEqual(pigLatin("egg"), "eggway");
+//     assert.strictEqual(pigLatin("tes3t5"), null);
+//   });
+// });
