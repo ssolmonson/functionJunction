4c9ca4ff561e1b850e781157d67e645f5f27c0b0
Scott Solmonson <ssolmonson63@gmail.com>
2025-11-20 14:53:26 -0500

    Create file for 'Friend or Foe?' excercise and add psuedocode of initial thoughts

---
+++ Python CodeWars/friendOrFoe.py
@@ -0,0 +1,14 @@
+# Kata Friend or Foe?
+# https://www.codewars.com/kata/55b42574ff091733d900002f/train/python
+
+def friend(x):
+    
+    # Create variable of empty list for friends to be added to
+    
+    # Loop over friend list 
+    #   for each friend split string into list
+    #   Compare length of string to 4
+    #   if equal to 4 append name to friend list
+
+    # return list of friends
+    pass
