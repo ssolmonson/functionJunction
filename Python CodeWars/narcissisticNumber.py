5877ba08ec47256df0a0b77ab7996efd5b175212
Scott Solmonson <ssolmonson63@gmail.com>
2025-11-20 10:15:00 -0500

    Add first thoughts as psuedocode

--- Python CodeWars/narcissisticNumber.py
+++ Python CodeWars/narcissisticNumber.py
@@ -1,2 +1,16 @@
 # Kata Narcissistic Number excercise
 # https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/python
+
+def narcissistic( value ):
+    # Count number of digits
+        # Store as a variable
+        # if number of digits equals 1 return True
+        # else store number as variable
+        
+    # Split digits
+    # Loop 
+        # Each digit is calculated to the power of the numOfDigits variable and added to the next
+        # Or multiply it against itself for the number  (could loop this)
+        
+    # Then compare looped value to original value and return true or false
+    return False # Code away
