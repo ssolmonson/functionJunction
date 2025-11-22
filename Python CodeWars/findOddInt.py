1bc079b22eea3c0bbfec6b6dcbcc8fa231eee826
Scott Solmonson <ssolmonson63@gmail.com>
2025-11-21 19:15:53 -0500

    Add file for and link to Kata, and add basic code and some initial rough thoughts/steps

---
+++ Python CodeWars/findOddInt.py
@@ -0,0 +1,7 @@
+# Kata Find Odd Int
+# https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+
+def find_it(seq):
+    # Loop over array
+    # If one value (int) equals another increase count
+    return None
