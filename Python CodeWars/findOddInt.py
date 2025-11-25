aca48f7e0e40cbf5bac6e888cd6f6ae86156f662
Scott Solmonson <ssolmonson63@gmail.com>
2025-11-25 15:23:30 -0500

    Add initial rough outline of code to test

--- Python CodeWars/findOddInt.py
+++ Python CodeWars/findOddInt.py
@@ -2,6 +2,15 @@
 # https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
 
 def find_it(seq):
+    intCount = 0
     # Loop over array
+    for i in seq:
     # If one value (int) equals another increase count
-    return None
+        if seq[i] == seq[i+1]:
+            intCount += 1
+            
+    if intCount != 0:
+        return intCount
+    else:
+        return -1
+    # return None
